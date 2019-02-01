68db12c7cb7b9e7fb97782d8d7873b81ff2d2b24
Mark Meretzky <mark.meretzky@gmail.com>
2019-02-01 11:19:08 -0500

    Commit.

--- HeartRateReceiver/ViewController.swift
+++ HeartRateReceiver/ViewController.swift
@@ -6,15 +6,45 @@
 //  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
-class ViewController: UIViewController {
+protocol HeartRateReceiverDelegate {
+    func heartRateUpdated(to bpm: Int);   //beats per minute
+}
+
+class HeartRateReceiver {
+    var currentHR: Int? {
+        didSet {
+            if let currentHR = currentHR {
+                print("The most recent heart rate reading is \(currentHR).")
+            } else {
+                print("Looks like we can't pick up a heart rate.")
+            }
+        }
+    }
+    
+    func startHeartRateMonitoringExample() {
+        for _ in 1 ... 10 {
+            let randomHR: Int = Int.random(in: 60 ..< 75);
+            currentHR = randomHR
+            Thread.sleep(forTimeInterval: 2)
+        }
+    }
+}
+class ViewController: UIViewController, HeartRateReceiverDelegate {
+    var heartRateLabel: UILabel = UILabel();
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let heartRateReceiver: HeartRateReceiver = HeartRateReceiver();
+        heartRateReceiver.startHeartRateMonitoringExample();
     }
-
-
+    
+    func heartRateUpdated(to bpm: Int) {
+        heartRateLabel.text = "\(bpm) bpm";
+        print("The user has been shown a heart rate of \(bpm) bpm.");
+    }
 }
 
